1c349d42745477b6ee67bf78e428517e29f1dc9b
Christopher Olsen <colsen@cboservices.mhs>
2020-07-18 07:14:00 -0600

    Update header / copyright

--- HangarControl.ino
+++ HangarControl.ino
@@ -1,4 +1,7 @@
 /*******************************************************************************
+ * Copyright (c) 2020 Christopher B. Olsen
+ * 
+ * LoRa code based on work by:
  * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
  *
  * Permission is hereby granted, free of charge, to anyone
@@ -7,9 +10,7 @@
  * including, but not limited to, copying, modification and redistribution.
  * NO WARRANTY OF ANY KIND IS PROVIDED.
  *
- * This example sends a valid LoRaWAN packet with payload "Hello,
- * world!", using frequency and encryption settings matching those of
- * the The Things Network.
+ * Remote PoWer Control and Hangar monitor LoRa application
  *
  * This uses OTAA (Over-the-air activation), where where a DevEUI and
  * application key is configured, which are used in an over-the-air
@@ -17,13 +18,9 @@
  * assigned/generated for use with all further communication.
  *
  * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
- * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
- * violated by this sketch when left running for longer)!
-
- * To use this sketch, first register your application and device with
- * the things network, to set or generate an AppEUI, DevEUI and AppKey.
- * Multiple devices can use the same AppEUI, but each device has its own
- * DevEUI and AppKey.
+ * g1, 0.1% in g2), but not the TTN fair usage policy.
+ * 
+ * Hangar Control application is registered on 'The Things Network'
  *
  * Do not forget to define the radio type correctly in config.h.
  *
